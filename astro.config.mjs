7d9ec08b65157d23c0fee04bde6a8c311232027a
lajbel <lajbelms@gmail.com>
2024-05-22 22:03:18 -0300

    build: remove image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +9,8 @@ export default defineConfig({
     redirects: {
         "/": "/guides/setup",
         "/guides": "/guides/setup",
+    },
+    image: {
+        service: passthroughImageService(),
     },
 });
