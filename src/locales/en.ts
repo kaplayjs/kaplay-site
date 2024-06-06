58c9440323d06a02be36f24f6eb689cfcf289025
lajbel <lajbelms@gmail.com>
2024-06-05 22:31:03 -0300

    chore: add ui i18n files

---
+++ src/locales/en.ts
@@ -0,0 +1,9 @@
+export const englishLocales = {
+    "sidebar": {
+        "home": "Home",
+        "playground": "Playground",
+        "guides": "Guides",
+        "refdoc": "Reference Docs",
+        "blog": "Blog",
+    },
+} as const;
--- src/util/i18n.ts
+++ src/util/i18n.ts
@@ -1,4 +1,6 @@
-export type Locale = "en" | "es";
+import { englishLocales } from "@/locales/en";
+
+export type Locale = typeof locales[number];
 export type LocaleMap = {
     [K in keyof typeof englishLocales]: {
         [K2 in keyof typeof englishLocales[K]]: string;
@@ -8,52 +10,19 @@ export type LocaleKeys = keyof LocaleMap;
 export type LocaleSubKeys = {
     [K in LocaleKeys]: keyof LocaleMap[K];
 };
-export type TranslationStringsHome = `home.${LocaleSubKeys["home"]}`;
+
+// Automate this
 export type TranslationStringsSidebar = `sidebar.${LocaleSubKeys["sidebar"]}`;
-export type TranslationStringsProjects =
-    `projects.${LocaleSubKeys["projects"]}`;
-export type TranslationString =
-    | TranslationStringsHome
-    | TranslationStringsSidebar
-    | TranslationStringsProjects;
+export type TranslationString = TranslationStringsSidebar;
 
-export const englishLocales = {
-    "home": {
-        "role": "Full Stack Web Developer",
-        "istudied":
-            "I studied Fullstack web development at the Technology University of Buenos Aires.",
-        "iworked":
-            "I've worked in different projects, from chat bot implementations for services to game engines, being maintener of KAPLAY.",
-    },
-    "sidebar": {
-        "description": "the online space",
-    },
-    "projects": {
-        "title": "Projects",
-    },
-} as const;
-
-export const spanishLocales = {
-    "home": {
-        "role": "Desarrollador Web Full Stack",
-        "istudied":
-            "Estudié desarrollo web Fullstack en la Universidad Tecnológica de Buenos Aires.",
-        "iworked":
-            "He trabajado en diferentes proyectos, desde implementaciones de chat bot para servicios hasta motores de juegos, siendo encargado de KAPLAY.",
-    },
-    "sidebar": {
-        "description": "the online space",
-    },
-    "projects": {
-        "title": "Proyectos",
-    },
-} as const;
+export const locales = ["en", "es"] as const;
+export const DEFAULT_LANG = "en";
 
 export const localesNames = ["en", "es"] as const;
 
 export const localesMap = {
     en: englishLocales,
-    es: spanishLocales,
+    es: null,
 };
 
 function splitLocaleKey(key: TranslationString) {
@@ -68,8 +37,10 @@ export function t(locale: Locale, key: TranslationString) {
     const localKey = keys[0];
     const localSubKey = keys[1];
     const localeMap = localesMap[locale];
+    const defaultLocaleMap = localesMap[locales[0]];
 
-    return localeMap[localKey][localSubKey];
+    localeMap?.[localKey]?.[localSubKey]
+        ?? defaultLocaleMap[localKey][localSubKey];
 }
 
 export function localedT(locale: Locale) {
