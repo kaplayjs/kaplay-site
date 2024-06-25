0d9ebec296b43bfdbf1b485d3d275105a79a858f
lajbel <lajbelms@gmail.com>
2024-06-25 15:35:18 -0300

    fix: links not in green

--- src/components/Sidebar/SidebarLink.tsx
+++ src/components/Sidebar/SidebarLink.tsx
@@ -1,6 +1,12 @@
 import type { Locale } from "@/util/i18n";
 import { getLangedRoute } from "@/util/path";
-import { component$, Slot, useTask$, useVisibleTask$ } from "@builder.io/qwik";
+import {
+    component$,
+    Slot,
+    useSignal,
+    useTask$,
+    useVisibleTask$,
+} from "@builder.io/qwik";
 import { isServer } from "@builder.io/qwik/build";
 
 type SidebarLinkProps = {
@@ -12,40 +18,19 @@ type SidebarLinkProps = {
 
 export const SidebarLink = component$((props: SidebarLinkProps) => {
     const lang = props.lang || "en";
+    const isCurrent = useSignal(false);
 
     useTask$(() => {
         if (isServer) {
             return; // Server guard
         }
 
-        const setUrl = () => {
-            const url = new URL(location.href);
-            const pathname = url.pathname;
-            const sidebar = document.querySelector<HTMLElement>(".drawer-side");
-            const links = document.querySelectorAll<HTMLElement>(
-                ".sidebar-link-a",
-            );
+        const url = new URL(location.href);
+        const pathname = url.pathname;
 
-            links.forEach((link) => {
-                const href = link.dataset.link;
-                link.classList.remove("btn-primary");
-                link.classList.add("btn-ghost");
-
-                if (href === pathname) {
-                    link.classList.remove("btn-ghost");
-                    link.classList.add("btn-primary");
-                    link.dataset.curScroll = sidebar?.scrollTop.toString();
-
-                    if (sidebar) {
-                        sidebar.scrollTop = link.offsetTop
-                            - sidebar.clientHeight / 2;
-                    }
-                }
-            });
-        };
-
-        document.addEventListener("astro:after-swap", () => setUrl());
-        document.addEventListener("DOMContentLoaded", () => setUrl());
+        if (pathname === props.link) {
+            isCurrent.value = true;
+        }
     });
 
     return (
