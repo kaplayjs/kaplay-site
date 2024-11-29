db42d7c7b755c6e4810a11beded3058cde231023
lajbel <lajbelms@gmail.com>
2024-11-29 17:35:08 -0300

    chore: fix ads

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,9 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        screens: {
+            "2sm": "380px",
+        },
     },
     plugins: [require("daisyui"), require("@tailwindcss/typography")],
     /** @type {import('daisyui').Config} */
