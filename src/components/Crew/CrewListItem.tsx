0a8706339ce17eb23b1d84a044ba986e6317b922
Daniel Báez <lajbelms@gmail.com>
2025-04-18 17:59:53 -0300

    feat: Some new features in Crew (#62)
    * feat: new crew page
    
    * chore: keep updating for new crew
    
    * chore: more updates
    
    * chore: some fmt

--- src/components/Crew/CrewListItem.tsx
+++ src/components/Crew/CrewListItem.tsx
@@ -9,17 +9,19 @@ export const CrewListItem = component$<CrewListItemProps>((props) => {
     const crewItem = assets[props.crewItem];
 
     return (
-        <div class="flex w-32 flex-col items-center justify-center rounded-box bg-base-100 p-4">
+        <div class="flex w-32 h-32 flex-col items-center justify-center rounded-box bg-base-100 p-4">
             <div>
                 <img
-                    src={crewItem.outlined}
+                    src={crewItem.outlined ?? crewItem.sprite}
                     alt={crewItem.name}
                     class="h-16 w-16 object-scale-down"
                 />
             </div>
 
             <div>
-                <h2 class="font-medium">{crewItem.name}</h2>
+                <h2 class="font-medium truncate max-w-[10ch]">
+                    {crewItem.name}
+                </h2>
             </div>
         </div>
     );
--- src/components/Sidebar/SidebarLink.tsx
+++ src/components/Sidebar/SidebarLink.tsx
@@ -1,31 +1,11 @@
-import apiIcon from "@/../assets/api_icon.png";
-import blogIcon from "@/../assets/blog_icon.png";
-import controllerIcon from "@/../assets/controller_icon.png";
-import discordIcon from "@/../assets/discord_icon.png";
-import donateIcon from "@/../assets/donate_icon.png";
-import githubIcon from "@/../assets/gh_icon.png";
-import guidesIcon from "@/../assets/guides_icon.png";
-import homeIcon from "@/../assets/home_icon.png";
-import pogIcon from "@/../assets/pog_icon.png";
 import { component$, Slot } from "@builder.io/qwik";
+import { assets } from "@kaplayjs/crew";
 
 type SidebarLinkProps = {
     href: string;
     target?: string;
     reloadAll?: boolean;
-    icon?: keyof typeof icons;
-};
-
-const icons = {
-    home: homeIcon,
-    github: githubIcon,
-    discord: discordIcon,
-    pog: pogIcon,
-    donate: donateIcon,
-    controller: controllerIcon,
-    blog: blogIcon,
-    guides: guidesIcon,
-    api: apiIcon,
+    icon?: keyof typeof assets;
 };
 
 export const SidebarLink = component$((props: SidebarLinkProps) => {
@@ -41,7 +21,7 @@ export const SidebarLink = component$((props: SidebarLinkProps) => {
             >
                 {props.icon && (
                     <img
-                        src={icons[props.icon].src}
+                        src={assets?.[props.icon].outlined}
                         alt="Home Icon"
                         class="h-6 w-6"
                     />
