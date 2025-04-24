e430998e544078196435b80707d52be37b578384
lajbel <71136486+lajbel@users.noreply.github.com>
2025-04-24 14:01:04 -0300

    chore: most land translated

---
+++ src/components/Testimonial/testimonials.ts
@@ -0,0 +1,72 @@
+export const testimonials = [
+    {
+        name: "Endel",
+        tagline: "Colyseus.io, Brawlball.io & Mazmorra.io creator",
+        comment:
+            "KAPLAY API is incredibly intuitive and easy to read. I love how it combines simplicity with power, it’s a truly innovative library that brings HTML5 games to life!",
+        social_link: "https://x.com/endel",
+        social_user: "endel",
+        social_type: "twitter",
+        avatar: "/kaplayers/endel.webp",
+        friend: "mark",
+        shape: "circle",
+    },
+    {
+        name: "Isaac Pante",
+        tagline: "Game Designer & College Teacher",
+        comment:
+            "KAPLAY is the best framework to teach programming and create fast and furious video games. We already won three awards with it, and counting!",
+        avatar: "/kaplayers/isaac.jpg",
+        friend: "dino",
+        shape: "squircle",
+        social_user: "ipante",
+        social_type: "mastodon",
+        social_link:
+            "https://gist.github.com/ipante/b5a1599a07db9a1671fe7a9887e85d24",
+    },
+    {
+        name: "JSLegend",
+        tagline: "JavaScript content creator",
+        comment:
+            "KAPLAY has a very intuitive and easy to use API that makes gamedev a breeze compared to other alternatives.",
+        social_user: "jslegenddev",
+        social_type: "youtube",
+        social_link: "https://youtube.com/@jslegenddev",
+        avatar: "/kaplayers/jslegend.webp",
+        friend: "bean",
+        shape: "squircle",
+    },
+    {
+        name: "Candy&Carmel",
+        tagline: "Representative of Ghosts community",
+        comment:
+            "KAPLAY has amazing and easy to use features for any type of game. It's made making FIGHT fun and interesting.",
+        avatar: "/kaplayers/candy.jpg",
+        friend: "ghosty",
+        shape: "squircle",
+    },
+    {
+        name: "Amy",
+        tagline: "Clickery Hexagon's developer",
+        comment:
+            "The development of KAPLAY has allowed me to make Clickery an awesome game.",
+        social_user: "amyspark-ng",
+        social_type: "newgrounds",
+        social_link: "https://amyspark-ng.newgrounds.com/",
+        avatar: "/kaplayers/amy.png",
+        friend: "mark",
+        shape: "hexagon",
+    },
+    {
+        name: "niceEli",
+        tagline: "Developer",
+        comment:
+            "KAPLAY is one of the best libraries for TypeScript I’ve tried. It’s a very simple API yet very powerful.",
+        social_user: "niceEli",
+        social_type: "mastodon",
+        social_link: "https://mastodon.gamedev.place/@niceEli",
+        avatar: "/kaplayers/niceEli.webp",
+        friend: "kat",
+        shape: "squircle",
+    },
+];
---
+++ src/components/Util/messages.ts
@@ -0,0 +1,28 @@
+export const defaultMessages = {
+    en: [
+        "And also..",
+
+        `We got the funkiest
+        <a
+            href="/crew"
+            target="_blank"
+            class="text-white decoration-2 underline decoration-white/20 underline-offset-2 hover:text-primary transition-colors"
+        >lore</a>
+        in the <span class="text-white">KAWorld</span>!`,
+
+        "Wow!",
+    ],
+    es: [
+        "Y también..",
+
+        `¡Tenemos la lore más funky
+        <a
+            href="/crew"
+            target="_blank"
+            class="text-white decoration-2 underline decoration-white/20 underline-offset-2 hover:text-primary transition-colors"
+        >de la pandilla</a>
+        en el <span class="text-white">KAWorld</span>!`,
+
+        "¡Apa la papa!",
+    ],
+};
