5a208b0e18a40b5c8a82a65dcf107ad46a06095b
imaginarny <gencurpeter@gmail.com>
2025-08-08 14:10:40 +0200

    chore: update open text to the same one as elsewhere

--- src/components/Crew/CrewItem.tsx
+++ src/components/Crew/CrewItem.tsx
@@ -424,7 +424,7 @@ export const CrewItem = (
                                     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
                                 </svg>
 
-                                Open in new page
+                                Open in new tab
                             </a>
                         </div>
                     )}
